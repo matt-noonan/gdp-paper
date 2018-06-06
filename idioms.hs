c9f23f5367c482e3a64b45859e31d455c4039a74
Matt Noonan <mnoonan@kataskeue.com>
2018-06-05 20:54:11 -0400

    Tweak the initial example a bit.

--- idioms.hs
+++ idioms.hs
@@ -7,38 +7,37 @@ head xs = case xs of
 endpts = do
   putStrLn "Enter a non-empty list of integers:"
   xs <- readLn
-  return (head xs, head $ reverse xs)
-
---------------------------------------------------------
+  if xs /= [] then return (head xs, head $ reverse xs)
+              else endpts
+----------------------------------------------------------
 -- Returning Maybe / Optional values. Safe, but requires
 -- the caller to pattern-match on the Maybe at every use,
 -- even when the list is known to be non-empty. Frustrated
 -- users cannot be blamed for using `fromJust`!
-safeHead :: [a] -> Maybe a
-safeHead xs = case xs of
+headMay :: [a] -> Maybe a
+headMay xs = case xs of
   (x:_) -> Just x
   []    -> Nothing
 
 safeEndpts = do
   putStrLn "Enter a non-empty list of integers:"
   xs <- readLn
-  case safeHead xs of
-    Just x -> return (x, fromJust (safeHead $ reverse xs))
+  case headMay xs of
+    Just x -> return (x, fromJust (headMay $ reverse xs))
     _      -> safeEndpts
-
--------------------------------------------------------
+----------------------------------------------------------
 -- "Ghosts of Departed Proofs". Safe. Does not return
 -- an optional value; preconditions are checked early
 -- and carried by "ghosts" (specialized phantom types).
 rev_cons :: IsCons xs -> Proof (IsCons (Reverse xs))
 
-gdpHead :: (a ~~ xs ::: IsCons xs) -> a
-gdpHead xs = head (the xs)
+gdpHead :: ([a] ~~ xs ::: IsCons xs) -> a
+gdpHead xs = head (the xs) -- safe!
 
 gdpEndpts = do
   putStrLn "Enter a non-empty list of integers:"
   xs <- readLn
   name xs $ \xs -> case classify xs of
-    Right is_cons -> let ok = is_cons |$ rev_cons in
+    IsCons evidence -> let ok = evidence >>= rev_cons in
       return (gdpHead xs, gdpHead (gdpRev xs ...ok))
-    Left  _  -> gdpEndpts
+    IsNil  evidence -> gdpEndpts
